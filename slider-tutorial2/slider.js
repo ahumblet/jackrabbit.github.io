1440772d7e06b1f55ed55b843a505cc0515bd4ab
Adrienne Humblet <amhumblet@gmail.com>
2020-06-24 17:32:21 -0700

    Implemented arrows in slider sandbox

--- slider-tutorial2/slider.js
+++ slider-tutorial2/slider.js
@@ -3,13 +3,34 @@
 $(document).ready(function() {
 
   $("#controls").on('click', 'span', function() {
+    var newImageIndex = $(this).index(); 
+
+    console.log($("#controls span.dot"));
+
 
     $("#slideshow img").removeClass("opaque");
-    var newImage = $(this).index();
-
-    $("#slideshow img").eq(newImage).addClass("opaque");
-    $("#controls span").removeClass("selected");
-    $(this).addClass("selected");
-    
+    $("#controls span.dot").removeClass("selected");
+    $("#slideshow img").eq(newImageIndex).addClass("opaque");
+    $("#controls span.dot").eq(newImageIndex).addClass("selected");
   });
+
+	$("#arrows").on('click', 'span', function() {
+			var arrowIndex = $(this).index(); 
+			var currImageIndex = $("#slideshow img").filter(".opaque").index();
+			var numImages = $("#slideshow img").length;
+			var newImageIndex;
+
+			if (arrowIndex == 0) {
+				newImageIndex = ((currImageIndex - 1) > 0) ? (currImageIndex - 1) : (numImages - 1);
+			} else if (arrowIndex == 1) {
+				newImageIndex = (currImageIndex + 1) % numImages;
+			}
+
+			$("#slideshow img").removeClass("opaque");
+			$("#controls span.dot").removeClass("selected");
+    	$("#slideshow img").eq(newImageIndex).addClass("opaque");
+    	$("#controls span.dot").eq(newImageIndex).addClass("selected");
+
+	  });
+
 });
